51d22e028ef3c99e25f209afb17c9a6e30924ddd
Logan Cox <lo5an@lo5an.com>
2019-06-18 00:15:12 -0500

    Add more docs and do some mild pylinting.

--- test_truss_normalize.py
+++ test_truss_normalize.py
@@ -2,23 +2,28 @@ import pytest
 from truss_normalize import *
 
 def test_normalize_timestamp():
-
+    # Check a Daylight Time converstion
     assert normalize_timestamp("4/1/11 11:00:00 AM") == "2011-04-01T14:00:00-04:00"
-    assert normalize_timestamp("1/1/11 12:00:01 AM") == "2011-01-01T03:00:01-05:00"
+    # Check a Standard Time conversion
     assert normalize_timestamp("1/1/11 12:00:01 AM") == "2011-01-01T03:00:01-05:00"
 
 def test_normalize_postal():
+    # Check padding
     assert normalize_postal("1") == "00001"
-
+    # Check nonconformant zipcode
     with pytest.raises(Exception):
         assert normalize_postal("123456")
 
 def test_normalize_fullname():
+    # Check a lowercase with diacritics
     assert normalize_fullname("EXáMPLE name") == "EXÁMPLE NAME"
+    # Check non-alphabetic
     assert normalize_fullname("株式会社スタジオジブリ") == "株式会社スタジオジブリ"
 
 def test_normalize_duration():
+    # Check seconds
     assert normalize_duration("0:0:6.0") == 6
+    # Check minutes
     assert normalize_duration("0:1:6.0") == 66
-    assert normalize_duration("0:1:6.0") == 66
+    # Check hours
     assert normalize_duration("10:1:6.0") == 36066
--- truss_normalize.py
+++ truss_normalize.py
@@ -6,10 +6,9 @@ import csv
 import datetime
 import pytz
 
-
-
 def normalize_timestamp(ts_text):
-
+    """ Converts "%m/%d/%y %I:%M:%S %p" timestamp in Pacific time to
+    in ISO-8601 timestamp in Eastern time  """
     time_fmt = "%m/%d/%y %I:%M:%S %p"
     time = datetime.datetime.strptime(ts_text, time_fmt)
     # Input timezone is US/Pacific
@@ -18,29 +17,34 @@ def normalize_timestamp(ts_text):
     # Desired output timezone is US/Eastern
     return time_pt.astimezone(pytz.timezone('US/Eastern')).isoformat()
 
-
 def normalize_postal(postal):
+    """Make sure postal codes are 5 digits and 0-padded, if needed."
+    """
     if (len(postal) > 5):
-        raise ValueError('Zip colume should contain 5 or fewer digits')
+        raise ValueError('Zip column should contain 5 or fewer digits')
     return postal.zfill(5)
 
 def normalize_fullname(fname):
+    """Make sure the FullName field is upper case only."""
     return fname.upper()
 
 def normalize_duration(dur_str):
-    hrs,mins,remainder = dur_str.split(":")
-    secs,mics = remainder.split(".")
+    """ Convert durations in HH:MM:SS.MS format (where MS is milliseconds)
+    to total seconds."""
+    hrs, mins, remainder = dur_str.split(":")
+    secs, mics = remainder.split(".")
 
-    dur = datetime.timedelta(hours = int(hrs),
-                             minutes = int(mins),
-                             seconds = int(secs),
-                             microseconds = int(mics))
-
+    dur = datetime.timedelta(hours=int(hrs),
+                             minutes=int(mins),
+                             seconds=int(secs),
+                             microseconds=int(mics))
 
     return dur.total_seconds()
 
 def truss_normalize(row):
-
+    """ Normalize a CSV row with the following columns:
+    Timestamp,Address,ZIP,FullName,FooDuration,BarDuration,TotalDuration,Notes
+    """
     timestamp = normalize_timestamp(row[0])
     address = row[1]
     postal = normalize_postal(row[2]) #  Zip in row headers
@@ -50,11 +54,13 @@ def truss_normalize(row):
     totalduration = fooduration+barduration
     notes = row[7]
 
-    normal_row = [ timestamp, address, postal, fullname, fooduration,
-                  barduration,totalduration,notes ]
-    return(normal_row)
+    normal_row = [timestamp, address, postal, fullname, fooduration,
+                  barduration, totalduration, notes]
+    return normal_row
 
 def main():
+    """Cleans up CSV rows from stdin using truss_normalize function,
+    and then prints to stdout."""
 
     csv_out = csv.writer(sys.stdout)
 
@@ -63,7 +69,7 @@ def main():
                                   encoding='utf-8',
                                   errors="replace")
 
-    # process line at a time, not filewise
+    # process line at a time, not file-wise
     csvlines = csv.reader(iter(utf8_stdin.readline, ''))
     for row in csvlines:
         try:
@@ -72,11 +78,11 @@ def main():
         except Exception as e:
             print('Error: %s' % e, file=sys.stderr)
 
-if __name__=='__main__':
+if __name__ == '__main__':
     rc = 1
     try:
         main()
-        rc=0
+        rc = 0
     except Exception as e:
         print('Error: %s' % e, file=sys.stderr)
     sys.exit(rc)
